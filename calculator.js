29b70e454de68cf5c3c1da8b11bad64237328d4e
Sameer <sameersultan1001@gmail.com>
2023-08-24 00:28:13 +0500

    calculator

---
+++ calculator.js
@@ -0,0 +1,4 @@
+import { Sub } from './sub';
+// let result=sum(2,2);
+let result = Sub(2, 2);
+console.log(result);
---
+++ calculator.ts
@@ -0,0 +1,6 @@
+import { sum } from './add'
+import { Sub } from './sub';
+// let result=sum(2,2);
+let result=Sub(2,2);
+
+console.log(result);
